67acab920ac989b70bd215cef8bd2b350b456768
adeelahmed <adeelahmedhashmi.aah@gmail.com>
2026-02-05 18:28:12 +0500

    fix(build): fix production build error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,7 +37,7 @@ function HomepageHeader() {
           <div className={styles.buttons}>
             <Link
               className="button button--primary button--lg"
-              to="docs/Overview">
+              to="/docs/overview">
               Get Started
             </Link>
           </div>
